ec8a813c459505c47c96b4f09877b93f86ccb5d9
Don Berge <DonBerge>
2023-08-02 12:49:13 -0300

    Changed about text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import Title from "./Title";
 
 const About = () => {
     return <div className="">
         <Title>Acerca de nosotros</Title>
         <p className="text-slate-200 text-justify text-xl md:w-3/5 m-auto">
-        <span className="font-bold text-orange-500">Persianas 247</span> es tu solución completa en Rosario para la instalación, reparación y atención inmediata de persianas, 
+        <span className="font-bold text-orange-500">Persianas 247</span> es tu solución completa en Rosario y alrededores para la instalación, reparación y atención inmediata de persianas, 
         disponibles las 24 horas. Con nuestro equipo profesional altamente capacitado, garantizamos resultados excepcionales y 
         nos enfocamos en la máxima satisfacción del cliente. Nos destacamos por nuestro compromiso con la rapidez y eficacia 
         en cada proyecto. Usamos herramientas y materiales de última generación para asegurar durabilidad en cada instalación 
