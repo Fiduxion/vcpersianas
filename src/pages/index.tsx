04154602c996c57303cebf5dd66fa1c47be75fb2
Don Berge <DonBerge>
2023-08-02 16:57:21 -0300

    Added favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,11 +9,20 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="font-poppins bg-slate-900">
+    <div className="font-poppins bg-slate-900 overflow-x-hidden">
       <Head>
         <title>Persianas 247</title>
         <meta charSet="utf-8"></meta>
-        <meta name="Pagina de venta de persianas con un formulario de contacto"></meta>
+        <meta name="description" content="Instalación, reparación y atención inmediata de persianas en Rosario y alrededores, disponibles las 24 horas"></meta>
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Banner />
       <div className="2xl:px-64 xl:px-20 sm:px-10 px-5 space-y-32">
@@ -23,7 +32,7 @@ export default function Home() {
         <ContactForm />
       </div>
       <Footer />
-      <WhatsAppButton/>
+      <WhatsAppButton />
     </div>
   );
 }
