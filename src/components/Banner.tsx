ef15a86b7d30a04c770949d4627c98aa0d089c95
Don Berge <DonBerge>
2023-08-05 13:56:17 -0300

    Fixed responsivenes error on banner and changed whatsapp link

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,7 @@
 const Banner = () => {
     return <div className="bg-slate-950 h-32 grid place-content-center rounded-b-3xl">
         <p className=" bg-gradient-to-r from-orange-500 to-red-600 bg-clip-text text-transparent font-semibold text-5xl md:text-6xl">Persianas 
-        <span className="font-black"> 24 horas</span></p>
+        <p className="font-black md:inline-flex ml-3"> 24 horas</p></p>
     </div>
 }
 
