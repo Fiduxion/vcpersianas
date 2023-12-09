0b0c180002869dbf1b19c05dd91fb2372944b6cd
DonBerge <agustinfb93@gmail.com>
2023-12-09 19:45:03 -0300

    Added conversions script to _document.tsx, whatsapp button now hides when the scroll passes a certain treshold

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 
 const WhatsAppButton = () => {
-    return <a aria-label="Boton flotante de Whatsapp" href="https://wa.me/5493413923576/?text=Hola%20tengo%20una%20urgencia.%20Me%20comunico%20desde%20la%20web.">
-        <div className="fixed flex items-center gap-3 bottom-5 right-5 md:bottom-10 md:right-10 bg-slate-950 p-5 rounded-full">
+    const [hideButton,setHideButton] = useState(false);
+
+    useEffect(() => {
+      document.addEventListener("scroll", () => {
+        const scrolled = document.scrollingElement!.scrollTop;
+        setHideButton(scrolled >= 2050);
+      });
+    }, []);
+
+    return <a className={hideButton ? "pointer-events-none cursor-default" : ""} aria-label="Boton flotante de Whatsapp" href="https://wa.me/5493413923576/?text=Hola%20tengo%20una%20urgencia.%20Me%20comunico%20desde%20la%20web.">
+        <div className={((hideButton) ? "opacity-0" : "opacity-100") + " fixed duration-700 transition-all opacity-100 flex items-center gap-3 bottom-5 right-5 md:bottom-10 md:right-10 bg-slate-950 p-5 rounded-full"}>
             <h1 className="hidden md:block text-slate-100 font-semibold md:text-2xl">Por urgencias <FaArrowRight className="inline-flex" /> </h1>
             <div className="grid">
                 <FaWhatsapp className=" text-slate-100 text-4xl md:text-5xl place-self-center" />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document'
- 
+ import Script from "next/script"; 
+
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11283475282" /> 
+        <Script id="google-analytics"> 
+          {`
+            window.dataLayer = window.dataLayer || []; 
+            function gtag(){dataLayer.push(arguments);} 
+            gtag('js', new Date()); gtag('config', 'AW-11283475282');
+          `}   
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
