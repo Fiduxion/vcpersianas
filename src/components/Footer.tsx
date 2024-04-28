af53aadbf3c564d9b6392526938646ce966eb243
DonBerge <agustinfb93@gmail.com>
2024-04-28 20:03:59 -0300

    Added phone numbers and runnned npm audit fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,16 @@ import { AiOutlineMail, AiFillEye } from "react-icons/ai";
 const Footer = () => {
     return <div className="py-16 bg-slate-950 justify-items-center flex md:flex-row flex-col justify-around items-center md:px-20 gap-10">
         <div className="text-orange-500 place-self-center font-bold text-4xl"><p>Persianas</p> <p className="ml-3">24 horas</p></div>
-        <div className="flex gap-16">
-            <a aria-label="Enviar Whatsapp" href="https://wa.me/5493412153361"><FaWhatsapp className="place-self-center text-slate-100" size={40}/></a>
-            <a aria-label="Enviar Email" href="mailto:persianaemiliano@gmail.com" ><AiOutlineMail className="place-self-center text-slate-100" size={40}/></a>
+        <div className="grid grid-cols-1 gap-y-4">
+            <div className="flex gap-16">
+                <a aria-label="Enviar Whatsapp" href="https://wa.me/5493412153361"><FaWhatsapp className="place-self-center text-slate-100" size={40}/></a>
+                <a aria-label="Enviar Email" href="mailto:persianaemiliano@gmail.com" ><AiOutlineMail className="place-self-center text-slate-100" size={40}/></a>
+            </div>
+            <div>
+                <p className="text-slate-200 text-center text-2xl font-bold">Telefonos</p>
+                <p className="text-slate-200 text-center text-xl">3412153361</p>
+                <p className="text-slate-200 text-center text-xl">341 215-3361</p>
+            </div>
         </div>
         <div className="flex items-center">
             <div className="place-self-center">
